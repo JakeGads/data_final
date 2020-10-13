63b1398a6d45f5274bb31ea400b012deb33aec0c
Jake Gadaleta <jake.gads@gmail.com>
2020-10-13 14:55:18 -0400

    fixed and optimized

--- clean_data.py
+++ clean_data.py
@@ -1,29 +1,26 @@
-from os import replace
-
-
+# opens up the raw data file
 with open ('ks.csv', 'r') as in_file:
-    records = in_file.readlines()
+    records = in_file.readlines() # loads all records into a list split by new lines (currently strings)
     
-    for i in range(len(records)):
-        records[i] = records[i].split(',')
-        
-        for h in ['', '\n']:
-            while h in records[i][15:]:
-                records[i].remove(h)
+    with open('clean_ks.csv', 'w+') as out_file: # opening an out file
+        for i in range(len(records)): # loads through each record
+            records[i] = records[i].split(',') # seperates the list by ,
+            for illegal_char in ['', '\n']: # checks if there are any characters their shouldn't be (either a new line or an empty)
+                while illegal_char in records[i][:15]: # while its in the valid range
+                    records[i][records[i].index(illegal_char)] = "N/A" # replace with a /n
+                while illegal_char in records[i][15:]: #while its past the maximum
+                    records[i].remove(illegal_char) # removes the characters past the limit
 
 
-    with open('clean_ks.csv', 'w+') as out_file:
-        for i in range(len(records)):
-            while len(records[i]) != len(records[0]):
-                records[i][1] += " " + records[i][2]
-                del records[i][2]
+            while len(records[i]) != len(records[0]): # checks if it is the proper size
+                records[i][1] += " " + records[i][2]  # if its not appends the split tittle
+                del records[i][2] # deletes the other (shifts everything back)
             
-            
-            for h in range(len(records[i])):
-                if h != 0:
+            for h in range(len(records[i])): # goes through the entire record
+                if h != 0: # skips the first so it doesn't add a first ','
                     out_file.write(',')
-                out_file.write(records[i][h])
-            out_file.write("\n")
+                out_file.write(records[i][h]) # writes out the actual record 
+            out_file.write("\n") # adds a \n to denote a new record
     
         
 
