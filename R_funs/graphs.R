b42e9f13507754d0b48c19cc39c4f44d2276b621
Jake Gadaleta <jakeg@localhost.localdomain>
2020-11-11 19:59:08 -0500

    syncing new graphs

--- R_funs/graphs.R
+++ R_funs/graphs.R
@@ -4,6 +4,25 @@ gen_regression <- function(df, col) {
         ggplot(df, mapping=aes_string(x="success", y=col)) +
         geom_point() +
         geom_smooth() + 
-        geom_ref_line()
+        geom_hline(yintercept=0)
     )
+}
+
+write_graph <- function(location, graph, web_image){
+    if(!file.exists(paste(file_name, '.png',sep = ""))){
+        png(paste(file_name, '.png', sep=''))
+        print(graph)
+        dev.off()
+    }
+    else {
+       print("  Already Generated PNG, to regen please clear data")
+    }
+    if(web_image && !file.exists(paste(file_name, '.svg',sep = ""))){
+        svg(paste(file_name, '.svg', sep=''))
+        print(graph)
+        dev.off()
+    }
+    else {
+       print("  Already Generated SVG, to regen please clear data")
+    }
 }
--- script.R
+++ script.R
@@ -1,31 +1,71 @@
-smart_package_loader <- function(package){
+# denotes weither or not you want to generate svg images (note it is a timely process)
+svg_gen = T
+
+#region smart loaders to load or download scripts
+smart_package_loader <- function(package, repo="http://cran.rstudio.com"){
     tryCatch({
         library(package, character.only=TRUE)
     },  error = function(e) {
-        install.packages(package, repos = "http://ftp.ussg.iu.edu/CRAN/", character.only=TRUE)
-        library(package)
+        install.packages(package, repos = repo)
+        tryCatch({
+                library(package, character.only=TRUE)
+            }, error = function(e) {
+                print("packages have failed to install please run in interactive mode")        
+            })  
     })
 }
 
-smart_script_loader <- function(location){
-    github <- "https://raw.githubusercontent.com/gadzygadz/data_final/main/"
-    tryCatch(
-        {
+smart_script_loader <- function(location, repo){
+    tryCatch({
             source(location)
         }, warning = function(e) {
             print(paste("Downloading ", location, sep= ""))
-            source_url(paste(github, location, sep=""))
-        }
-    )
+            source_url(paste(repo, location, sep=""))
+        }, error = function(e) {
+            print("must use the raw page ie raw.githubusercontent.com, instead of github.com")
+        })
 }
 
-for (i in c("tidyverse", "devtools")){
-    smart_package_loader(i)
+
+for (i in c("tidyverse", "devtools", "modelr")){
+    smart_package_loader(i, "http://ftp.ussg.iu.edu/CRAN/") # on DeSales wifi this has been the best cran mirror to use
 }
 
 for (i in c("R_funs/graphs.R", "R_funs/smart_data.R")){
-    smart_script_loader(i)
+    smart_script_loader(i, "https://raw.githubusercontent.com/gadzygadz/data_final/main/")
 }
+#endregion
 
-df <- get_data("clean_ks.csv")
+#region Column Information for generating graphs
+'
+ [1] "ID"               "name"             "category"         "main_category"   
+ [5] "currency"         "deadline"         "goal"             "launched"        
+ [9] "pledged"          "state"            "backers"          "country"         
+[13] "usd_pledged"      "usd_pledged_real" "usd_goal_real"
+'
+#endregion
 
+#region configuring the dataframe
+df <- get_data("clean_ks.csv") 
+df <- df %>%
+mutate(success=usd_pledged_real - usd_goal_real)
+#endregion
+
+#region regression image generators
+dir.create("regresion_imgs")
+
+count = 0
+for(i in colnames(df)){
+    file_name <- paste('regresion_imgs/', 0, count, '_', i, sep='')
+    if (count > 9) {file_name <- paste('regresion_imgs/', count, '_', i, sep='')}
+    print(file_name)
+    count = count + 1
+    graph <- gen_regression(df, i)
+    write_graph(file_name, graph, svg_gen)
+}
+#endregion
+
+# Jake
+## After examing all the generated regressions it quickly struck me that a lot of these projects did successed which I felt was shocking at this point I wanted to by how much did they successed by
+
+# We that making a scatterplot and 
