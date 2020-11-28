a2415cf9f69cbdc3684b738abdece84d8240830c
Jake Gadaleta <jake.gads@gmail.com>
2020-11-28 11:53:05 -0500

    mutated

--- Data Anayltics/add_success_factor.py
+++ Data Anayltics/add_success_factor.py
@@ -3,25 +3,43 @@ ID,name,category,main_category,currency,deadline,goal,launched,pledged,state,bac
 """
 
 class Row:
-    def __init__(self, ID,name,category,main_category,currency,deadline,goal,launched,pledged,state,backers,country,usd_pledged,usd_pledged_real,usd_goal_real):
-        self.ID = ID
-        self.name = name
-        self.category = category
-        self.main_category = main_category
-        self.currency = currency
-        self.deadline = deadline
-        self.goal = goal
-        self.launched = launched
-        self.pledged = pledged
-        self.state = state
-        self.backers = backers
-        self.country = country
-        self.usd_pledged = usd_pledged
-        self.usd_pledged_real = usd_pledged_real
-        self.usd_goal_real = usd_goal_real
-        try:
-            self.success_factor = usd_pledged_real / usd_goal_real
+    def __init__(self, entry):
+        self.ID =entry[0]
+        self.name =entry[1]
+        self.category =entry[2]
+        self.main_category =entry[3]
+        self.currency =entry[4]
+        self.deadline =entry[5]
+        self.goal =entry[6]
+        self.launched =entry[7]
+        self.pledged =entry[8]
+        self.state =entry[9]
+        self.backers =entry[10]
+        self.country =entry[11]
+        self.usd_pledged =entry[12]
+        self.usd_pledged_real=""
+        self.usd_goal_real = ""
+        self.success_factor = ""
         
+        try:
+            self.usd_pledged_real=float(entry[13])
+            self.usd_goal_real =float(entry[14][:-1])
+        except Exception:
+            self.usd_pledged_real=entry[13]
+            self.usd_goal_real =entry[14][:-1]
+        try:
+            self.success_factor = (self.usd_pledged_real+1) / (1+self.usd_goal_real)
+        except Exception:
+            self.success_factor = "success_factor"
 
-    def __repr__():
-        return '{self.ID},{self.name},{self.category},{self.main_category},{self.currency},{self.deadline},{self.goal},{self.launched},{self.pledged},{self.state},{self.backers},{self.country},{self.usd_pledged},{self.usd_pledged_real},{self.usd_goal_real}{self.sucess_factor}'
+    def __repr__(self):
+        return f'{self.ID},{self.name},{self.category},{self.main_category},{self.currency},{self.deadline},{self.goal},{self.launched},{self.pledged},{self.state},{self.backers},{self.country},{self.usd_pledged},{self.usd_pledged_real},{self.usd_goal_real},{self.success_factor}\n'
+
+with open("clean_ks.csv", "r") as in_file:
+    with open("clean_ks_app.csv", "w+") as out_file:
+        for i in in_file.readlines():
+            try:
+                out_file.write(Row(i.split(",")).__repr__())
+                
+            except Exception:
+                print(i)
